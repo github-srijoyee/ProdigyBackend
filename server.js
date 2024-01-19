3180de7827ab7cde2b549f3afe2a5aaf5ade1c26
github-srijoyee <137808302+github-srijoyee@users.noreply.github.com>
2024-01-19 20:00:55 +0530

    important

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const errorMiddleware = require("./middleware/error-middleware");
 
 //let's tackle cors
 const corsOptions={
-    origin:`http://localhost:5173`,
+    origin:`https://prodigyapp.netlify.app/`,
     methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials:true,
 }
