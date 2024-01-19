f2b238e83d231be2a95f1cbd68bd1ce0e7cea568
github-srijoyee <137808302+github-srijoyee@users.noreply.github.com>
2024-01-19 15:18:25 +0530

    ..

--- server.js
+++ server.js
@@ -39,12 +39,12 @@ app.use("/api/form",contactRoute);
 app.use("/api/data",serviceRoute);
 
 //let's define admin route
-app.use("/api/admin",adminRoute);
+app.use("/api/admin",cors(corsOptions),adminRoute);
 
 app.use(errorMiddleware);
 
 
-    const PORT=5000;
+    const PORT=process.env.PORT||5000;
 
     connectDb().then(()=>{
         app.listen(PORT,()=>{
